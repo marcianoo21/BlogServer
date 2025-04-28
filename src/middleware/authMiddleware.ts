e8bfdb4b6ffdb7667a735beead2244590b2586c1
marcianoo21 <mgrzelak218@gmail.com>
2025-04-28 02:43:30 +0200

    auth middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import  { Response, NextFunction } from 'express'
+import { Schema } from 'mongoose';
+
+
+const middleAuth = (req: any, res: Response, next: NextFunction) => {
+    const authToken = req.headers.authorization;
+
+    if (!authToken || !authToken.startsWith('Bearer ')) {
+        res.status(400).json({ message: "Your token has expired or it is invalid!" });
+        return;
+    }
+
+    const token = authToken.split(' ')[1];
+
+    try {
+        const decodedData = jwt.verify(token, 'your-secret-key') as {userId: string, username: Schema.Types.ObjectId };
+        console.log("Decoded token:", decodedData);
+        req.user = decodedData;
+        next();
+    } catch (err: any) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+export default middleAuth;
