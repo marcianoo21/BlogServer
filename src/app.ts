b7ce84320da074d74d4566ce5f45bd1f038613a0
marcianoo21 <mgrzelak218@gmail.com>
2025-04-28 02:42:19 +0200

    middleware for posts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import connectDB from "./config/db";
+import userRouter from "./controllers/authController"
+import middleAuth from './middleware/authMiddleware';
 const cors = require('cors')
 const app = express()
 const port = 3000
@@ -10,7 +12,8 @@ app.use(express.json())
 app.use(cors())
 
 const postsRouter = require('./routes/posts')
-app.use('/posts', postsRouter)
+app.use('/posts', middleAuth, postsRouter)
+app.use('/auth', userRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
