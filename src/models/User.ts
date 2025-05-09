d46bbad2933c6893c38735d54148a0f7a02bfb5a
marcianoo21 <mgrzelak218@gmail.com>
2025-05-09 23:59:13 +0200

    roles added

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,7 +13,7 @@ const UserSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  role: { type: String, enum: ['admin', 'author', 'reader'], default: 'reader' }
+  role: { type: String, enum: ['admin', 'user'], default: 'user' }
 });
 
 export default mongoose.model<IUser>("User", UserSchema);
