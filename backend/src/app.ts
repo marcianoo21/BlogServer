a34cbcefd48fb6aeef333edfb31cf5cc8d7e9073
marcianoo21 <mgrzelak218@gmail.com>
2025-06-08 23:47:14 +0200

    import syntax changed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,23 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 import connectDB from "./config/db";
-import { userRouter } from './routes/auth'
+import { userRouter } from './routes/auth';
 import middleAuth from './middleware/authMiddleware';
-import { postRouter } from './routes/posts'
+import { postRouter } from './routes/posts';
 
-// npx ts-node-dev src/app.ts
-const cors = require('cors')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 connectDB();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/posts', middleAuth, postRouter)
-app.use('/auth', userRouter)
+app.use('/posts', middleAuth, postRouter);
+app.use('/auth', userRouter);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
+// npx ts-node src/app.ts
 
