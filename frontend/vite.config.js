8f17dc694e54be5b5b2b1a6d38e3cfe39e4d4ca6
marcianoo21 <mgrzelak218@gmail.com>
2025-06-30 12:33:11 +0200

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
