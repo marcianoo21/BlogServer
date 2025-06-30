322d4cc1ea960a9c456a27a8d40ef37d2846612c
marcianoo21 <mgrzelak218@gmail.com>
2025-06-30 12:43:31 +0200

    import changed again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login, Main, Test } from '../src/pages';
-import { ProtectedRoute } from './components/ProtectedRoute'
+import { ProtectedRoute } from './components/ProtectedRoute.jsx'
 
 function App() {
   return (
